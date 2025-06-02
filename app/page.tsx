d8e8f0caeb3f7bbb44d33c86366d6300bf0691fc
vitaliyis <vetal.friend@gmail.com>
2025-06-02 14:07:01 +0300

    Change Input component to use defaultValue instead of value for better form handling

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className="mt-10">
       <div className="text-red-500 text-2xl flex">
-        <Input type="text" placeholder="Enter text" value="Hello World" />
+        <Input type="text" placeholder="Enter text" defaultValue="Hello World" />
         <Button>Click me</Button>
       </div>
     </div>
