5cb2689f4dd1b8775a85ef5ca703218911b5c7f4
vitaliyis <vetal.friend@gmail.com>
2025-06-03 15:04:14 +0300

    Integrate Prisma adapter with NextAuth and update secret environment variable

--- auth.ts
+++ auth.ts
@@ -1,15 +1,17 @@
 import NextAuth from "next-auth";
+import { PrismaAdapter } from "@auth/prisma-adapter";
 import { authConfig } from "./auth.config";
+import { prisma } from "./lib/prisma";
 
 export const { 
   handlers, 
   auth, 
   signIn, 
-  signOut,
-  update 
+  signOut
 } = NextAuth({
+  adapter: PrismaAdapter(prisma),
   ...authConfig,
   session: { strategy: "jwt" },
-  secret: process.env.AUTH_SECRET || "your-super-secret-key-that-should-be-in-env",
+  secret: process.env.NEXTAUTH_SECRET || "your-super-secret-key-that-should-be-in-env",
   debug: process.env.NODE_ENV === "development",
 }); 
