eeda942ff9d65da1e03e6ac666a31705a49a0a4f
vitaliyis <vetal.friend@gmail.com>
2025-06-16 15:52:55 +0300

    add video page

---
+++ app/video/page.tsx
@@ -0,0 +1,16 @@
+'use client';
+
+import React from 'react';
+
+const VideoPage = () => {
+  return (
+    <div className="container mx-auto p-4">
+      <h1 className="text-2xl font-bold mb-4">Видео</h1>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        {/* Здесь будет контент */}
+      </div>
+    </div>
+  );
+};
+
+export default VideoPage; 
